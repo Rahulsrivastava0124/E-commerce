a0e5fcf67360a0ca1f71652607ebd1c816927242
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2023-07-13 22:27:29 +0530

    rating in product but fill and blank not half is pending

--- src/Component/Categories.jsx
+++ src/Component/Categories.jsx
@@ -16,7 +16,6 @@ export const Categories = (props) => {
   }, [props.data]);
 
   console.log(ResData);
-
   return (
     <>
       <h3 className="fw-bolder text-center">
@@ -24,32 +23,87 @@ export const Categories = (props) => {
         {props.data ? props.data : null}{" "}
       </h3>
       <div className="container d-flex flex-wrap">
-        {Array.isArray(ResData)
-          ? ResData.map((element, tabIndex) => {
-              return (
-                <div
-                  className="card m-2"
-                  style={{ width: "18rem" }}
-                  key={tabIndex}
-                >
-                  <img
-                    src={element.image}
-                    className="card-img-top m-2"
-                    alt="..."
-                    style={{ width: "250px", height: " 250px" }}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{element.title}</h5>
-                    <p className="card-text">{element.description}</p>
-                    <h5> Rs.{element.price} </h5>
-                    <a href="/" className="btn btn-primary">
-                      Buy
-                    </a>
+        {Array.isArray(ResData) ? (
+          ResData.map((element, tabIndex) => {
+            let Rating = Number.parseFloat(element.rating.rate).toFixed(0);
+            {
+              var rateArray = ["", "", "", "", ""];
+
+              /* let RatingDecimaltoString = element.rating.rate.toString().slice();
+            let RatingDecimal=Number.parseFloat(RatingDecimaltoString).toFixed(0) */
+            }
+            {
+              /* console.log('====================================');
+            console.log(RatingDecimal);
+            console.log(RatingDecimaltoString);
+            console.log('===================================='); */
+            }
+
+            for (let i = 0; i < Rating; i++) {
+              rateArray[i] = "-fill";
+            }
+            {
+              /* if (RatingDecimal === "1") {
+              i++;
+              rateArray[i] = "-half";
+            } */
+            }
+
+            console.log("====================================");
+            console.log(rateArray);
+            console.log("====================================");
+            return (
+              <div
+                className="card m-3 border-0 shadow-sm"
+                style={{ width: "18rem" }}
+                key={tabIndex}
+              >
+                <img
+                  className="mx-auto pt-3 "
+                  style={{ width: "250px", height: "200px" }}
+                  src={element.image}
+                  alt="image"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{element.title.slice(0, 20)}</h5>
+                  <p className="card-text">
+                    {element.description.slice(0, 50)}
+                  </p>
+                  <div className="d-flex justify-content-between">
+                    <h3 className="fw-bold text-primary">Rs.{element.price}</h3>
+                    <h7 className="text-success"> Instock </h7>
                   </div>
+                  <h6 className="text-decoration-line-through">
+                    Rs.{element.price + 300}
+                  </h6>
+                  <h5>
+                    {rateArray.map((element, index) => {
+                      return (
+                        <i className={`bi bi-star${element}`} key={index}></i>
+                      );
+                    })}
+                    {/* <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-half"></i>
+                    <i className="bi bi-star"></i>
+                    <i className="bi bi-star"></i> */}
+                  </h5>
+                  <a href="/" className="btn btn-primary">
+                    Add to cart
+                  </a>
+                  <button type="button" className="btn btn-info mx-2">
+                    <i className="bi bi-heart-fill"></i>
+                  </button>
+                  <button type="button" className="btn btn-danger">
+                    <i className="bi bi-eye-fill"></i>
+                  </button>
                 </div>
-              );
-            })
-          : null}
+              </div>
+            );
+          })
+        ) : (
+          <h1>Loading</h1>
+        )}
       </div>
     </>
   );
