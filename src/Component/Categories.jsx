0b0478e6451a3751aba70a9989c0e6a571089841
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2023-07-14 23:29:02 +0530

    add wish-list Click effect in product card

--- src/Component/Categories.jsx
+++ src/Component/Categories.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import solidColorImage from "../image/solidColorImage.png";
 export const Categories = (props) => {
   const [ResData, setResData] = useState("");
 
@@ -26,16 +26,30 @@ export const Categories = (props) => {
             let rating = element.rating.rate.toString().split(".");
             let dec = `0.${rating[1]}`;
             let decRating = parseFloat(dec).toFixed();
-
             var rateArray = ["", "", "", "", ""];
-
             for (var i = 0; i < Number(rating[0]); i++) {
               rateArray[i] = "-fill";
             }
             if (decRating === "1") {
               rateArray[i] = "-half";
             }
+            const WishList = (e) => {
+              if (e.target.classList[0] === "btn") {
+                var data = e.target.firstChild.classList[1];
+              } else {
+                data = e.target.classList[1];
+              }
 
+              let ClassData = document.querySelector(`.${data}`);
+
+              if (ClassData.classList.contains("bi-heart")) {
+                ClassData.classList.add("bi-heart-fill");
+                ClassData.classList.remove("bi-heart");
+              } else {
+                ClassData.classList.remove("bi-heart-fill");
+                ClassData.classList.add("bi-heart");
+              }
+            };
             return (
               <div
                 className="card m-3 border-0 shadow-sm"
@@ -46,7 +60,7 @@ export const Categories = (props) => {
                   className="mx-auto pt-3 "
                   style={{ width: "250px", height: "200px" }}
                   src={element.image}
-                  alt="image"
+                  alt="productImage"
                 />
                 <div className="card-body">
                   <h5 className="card-title">{element.title.slice(0, 20)}</h5>
@@ -66,18 +80,19 @@ export const Categories = (props) => {
                         <i className={`bi bi-star${element}`} key={index}></i>
                       );
                     })}
-                    {/* <i className="bi bi-star-fill"></i>
-                    <i className="bi bi-star-fill"></i>
-                    <i className="bi bi-star-half"></i>
-                    <i className="bi bi-star"></i>
-                    <i className="bi bi-star"></i> */}
                   </h5>
                   <a href="/" className="btn btn-primary">
                     Add to cart
                   </a>
-                  <button type="button" className="btn btn-info mx-2">
-                    <i className="bi bi-heart-fill"></i>
-                  </button>
+                  <span
+                    type="button"
+                    className="btn ScaleButton mx-2 rounded-circle shadow "
+                    onClick={(e) => WishList(e)}
+                  >
+                    <i
+                      className={`IconScale  ${element.category}${tabIndex} bi bi-heart fw-bolder `}
+                    ></i>
+                  </span>
                   <button type="button" className="btn btn-danger">
                     <i className="bi bi-eye-fill"></i>
                   </button>
@@ -86,7 +101,168 @@ export const Categories = (props) => {
             );
           })
         ) : (
-          <h1>Loading</h1>
+          <div className=" container d-flex flex-wrap">
+            <div
+              className="card m-2"
+              style={{ width: "18rem" }}
+              aria-hidden="true"
+            >
+              <img src={solidColorImage} alt="" />
+              <div className="card-body">
+                <h5 className="card-title placeholder-glow">
+                  <span className="placeholder col-6"></span>
+                </h5>
+                <p className="card-text placeholder-glow">
+                  <span className="placeholder col-7"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-8"></span>
+                </p>
+                <span className="btn btn-primary disabled placeholder col-6"></span>
+              </div>
+            </div>
+            <div
+              className="card m-2"
+              style={{ width: "18rem" }}
+              aria-hidden="true"
+            >
+              <img src={solidColorImage} alt="" />
+              <div className="card-body">
+                <h5 className="card-title placeholder-glow">
+                  <span className="placeholder col-6"></span>
+                </h5>
+                <p className="card-text placeholder-glow">
+                  <span className="placeholder col-7"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-8"></span>
+                </p>
+                <span className="btn btn-primary disabled placeholder col-6"></span>
+              </div>
+            </div>
+            <div
+              className="card m-2"
+              style={{ width: "18rem" }}
+              aria-hidden="true"
+            >
+              <img src={solidColorImage} alt="" />
+              <div className="card-body">
+                <h5 className="card-title placeholder-glow">
+                  <span className="placeholder col-6"></span>
+                </h5>
+                <p className="card-text placeholder-glow">
+                  <span className="placeholder col-7"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-8"></span>
+                </p>
+                <span className="btn btn-primary disabled placeholder col-6"></span>
+              </div>
+            </div>
+            <div
+              className="card m-2"
+              style={{ width: "18rem" }}
+              aria-hidden="true"
+            >
+              <img src={solidColorImage} alt="" />
+              <div className="card-body">
+                <h5 className="card-title placeholder-glow">
+                  <span className="placeholder col-6"></span>
+                </h5>
+                <p className="card-text placeholder-glow">
+                  <span className="placeholder col-7"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-8"></span>
+                </p>
+                <span className="btn btn-primary disabled placeholder col-6"></span>
+              </div>
+            </div>
+            <div
+              className="card m-2"
+              style={{ width: "18rem" }}
+              aria-hidden="true"
+            >
+              <img src={solidColorImage} alt="" />
+              <div className="card-body">
+                <h5 className="card-title placeholder-glow">
+                  <span className="placeholder col-6"></span>
+                </h5>
+                <p className="card-text placeholder-glow">
+                  <span className="placeholder col-7"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-8"></span>
+                </p>
+                <span className="btn btn-primary disabled placeholder col-6"></span>
+              </div>
+            </div>
+            <div
+              className="card m-2"
+              style={{ width: "18rem" }}
+              aria-hidden="true"
+            >
+              <img src={solidColorImage} alt="" />
+              <div className="card-body">
+                <h5 className="card-title placeholder-glow">
+                  <span className="placeholder col-6"></span>
+                </h5>
+                <p className="card-text placeholder-glow">
+                  <span className="placeholder col-7"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-8"></span>
+                </p>
+                <span className="btn btn-primary disabled placeholder col-6"></span>
+              </div>
+            </div>
+            <div
+              className="card m-2"
+              style={{ width: "18rem" }}
+              aria-hidden="true"
+            >
+              <img src={solidColorImage} alt="" />
+              <div className="card-body">
+                <h5 className="card-title placeholder-glow">
+                  <span className="placeholder col-6"></span>
+                </h5>
+                <p className="card-text placeholder-glow">
+                  <span className="placeholder col-7"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-8"></span>
+                </p>
+                <span className="btn btn-primary disabled placeholder col-6"></span>
+              </div>
+            </div>
+            <div
+              className="card m-2"
+              style={{ width: "18rem" }}
+              aria-hidden="true"
+            >
+              <img src={solidColorImage} alt="" />
+              <div className="card-body">
+                <h5 className="card-title placeholder-glow">
+                  <span className="placeholder col-6"></span>
+                </h5>
+                <p className="card-text placeholder-glow">
+                  <span className="placeholder col-7"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-8"></span>
+                </p>
+                <span className="btn btn-primary disabled placeholder col-6"></span>
+              </div>
+            </div>
+          </div>
         )}
       </div>
     </>
