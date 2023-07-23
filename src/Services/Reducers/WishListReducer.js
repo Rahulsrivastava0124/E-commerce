1452402f138555fda9600f428fe9c8e33131e92a
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2023-07-23 13:46:41 +0530

    WishList add and remove function in store Is Completed

--- src/Services/Reducers/WishListReducer.js
+++ src/Services/Reducers/WishListReducer.js
@@ -1,21 +1,25 @@
-import {ADD_TO_WISHLIST,REMOVE_TO_WISHLIST} from '../constant'
-  
-export const UserWish=(state=[],action)=>{
-    switch (action.type) {
-      case ADD_TO_WISHLIST:
-        return [
-          ...state,
-         { WishList: action.data,}
-        ];
-        case REMOVE_TO_WISHLIST:
-          console.log("state",state);
-          state.pop();
-          return [
-            ...state,
-          ]
-    
-      default:
-        return state;
-       
-    }
-    }
+import { ADD_TO_WISHLIST, REMOVE_TO_WISHLIST } from "../constant";
+
+export const UserWish = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TO_WISHLIST:
+      return [...state, { WishList: action.data }];
+    case REMOVE_TO_WISHLIST:
+      console.log("action", state);
+      console.log("Action data", action.data);
+      for (let index = 0; index < state.length; index++) {
+        if (
+          state[index].WishList.state.category === action.data.state.category
+        ) {
+          console.log("first", state[index]);
+          if (state[index].WishList.state.id === action.data.state.id) {
+            state.splice(index, index);
+          }
+        }
+      }
+      return [...state];
+
+    default:
+      return state;
+  }
+};
