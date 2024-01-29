2a175180d10c3f4a9e4efa510d1c20a9a5b5a8a3
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-01-29 18:31:10 +0530

    Update login form UI.

--- frontend/src/Component/Navbar/Login.jsx
+++ frontend/src/Component/Navbar/Login.jsx
@@ -58,79 +58,81 @@ function Login(props) {
 
             {loading ? <Loading /> :
 
-              <div className="modal-body p-0 ">
-                <div className="d-flex justify-content-around align-items-center">
+              <div className="modal-body p-0  ">
+                <div className="d-flex align-items-center justify-content-between">
                   <div className="w-50 ">
                     <img src={LoginLogo} alt="login logo" style={{ width: "-webkit-fill-available" }} className="rounded-3" />
                   </div>
-                  <form className="pe-3 pt-3 w-auto">
-                    <h3 className="text-primary">Login</h3>
-                    <div className="mb-3 text-start mt-4">
-                      <label
-                        htmlFor="exampleInputUserName"
-                        className="form-label"
-                      >
-                        User Name
-                      </label>
-                      <input
-                        type="UserName"
-                        className="form-control"
-                        id="exampleInputUserName"
-                        aria-describedby="emailHelp"
-                        value={LoginData.email}
-                        required
-                        onChange={(e) =>
-                          setLoginData({ ...LoginData, email: e.target.value })
-                        }
-                      />
-                      <div id="emailHelp" className="form-text">
-                        We'll never share your email with anyone else.
+                  <div className="bg-body-secondary rounded-end-3">
+                    <form className="pe-3 pt-3 w-auto px-5">
+                      <h3 className="text-primary">Login</h3>
+                      <div className="mb-3 text-start mt-4">
+                        <label
+                          htmlFor="exampleInputUserName"
+                          className="form-label"
+                        >
+                          User Name
+                        </label>
+                        <input
+                          type="UserName"
+                          className="form-control"
+                          id="exampleInputUserName"
+                          aria-describedby="emailHelp"
+                          value={LoginData.email}
+                          required
+                          onChange={(e) =>
+                            setLoginData({ ...LoginData, email: e.target.value })
+                          }
+                        />
+                        <div id="emailHelp" className="form-text">
+                          We'll never share your email with anyone else.
+                        </div>
                       </div>
-                    </div>
-                    <div className="mb-2 text-start">
-                      <label
-                        htmlFor="exampleInputPassword1"
-                        className="form-label"
-                      >
-                        Password
-                      </label>
-                      <input
-                        type={user_login_password_show ? "text" : "password"}
-                        className="form-control"
-                        id="exampleInputPassword1"
-                        value={LoginData.password}
-                        required
-                        onChange={(e) =>
-                          setLoginData({ ...LoginData, password: e.target.value })
-                        }
-                      />
-                      <i class={`bi bi-${user_login_password_show ? 'eye-slash' : "eye"} ${user_login_password_show ? 'text-primary' : "text-dark"} text-primary h5`} id="user_login_password_show" onClick={() => setuser_login_password_show(!user_login_password_show)}></i>
-                    </div>
-                    <div className="d-flex justify-content-between">
-                      <div class="form-check form-switch mb-3">
-                        <input class="form-check-input" type="checkbox" style={{ cursor: "pointer" }} role="switch" id="flexSwitchCheckDefault" />
-                        <label class="form-check-label form-text" for="flexSwitchCheckDefault">Remember me</label>
+                      <div className="mb-2 text-start">
+                        <label
+                          htmlFor="exampleInputPassword1"
+                          className="form-label"
+                        >
+                          Password
+                        </label>
+                        <input
+                          type={user_login_password_show ? "text" : "password"}
+                          className="form-control"
+                          id="exampleInputPassword1"
+                          value={LoginData.password}
+                          required
+                          onChange={(e) =>
+                            setLoginData({ ...LoginData, password: e.target.value })
+                          }
+                        />
+                        <i class={`bi bi-${user_login_password_show ? 'eye-slash' : "eye"} ${user_login_password_show ? 'text-primary' : "text-dark"} text-primary h5`} id="user_login_password_show" onClick={() => setuser_login_password_show(!user_login_password_show)}></i>
                       </div>
-                      <span className="text-primary form-text" style={{ cursor: "pointer" }}> Forget password ?</span>
-                    </div>
-                    <div className="d-grid container">
-                      <button
-                        type="submit"
-                        className={`btn btn-primary me-2 fw-bold ${loading || (LoginData.email == "" && LoginData.password == "") ? "disabled" : null
-                          }`}
-                        onClick={(e) => SubmitLoginForm(e)}
-                      >
-                        Login
-                      </button>
-                    </div>
-                    <h6 className="d-flex justify-content-evenly my-3 form-text"><span>Don't have an account?</span><span className="text-primary" style={{ cursor: "pointer" }} data-bs-target="#SignUpModelToggle2"
-                      data-bs-toggle="modal" >Sign Up</span>
-                    </h6>
-                    <div className="d-flex justify-content-evenly my-4">
-                      <button className=" btn btn-light border"><i class="bi bi-google text-primary px-2"></i>Google</button>
-                      <button className=" btn btn-light border"><i class="bi bi-apple text-secondary px-2"></i>Apple</button>
-                    </div>
-                  </form>
+                      <div className="d-flex justify-content-between">
+                        <div class="form-check form-switch mb-3">
+                          <input class="form-check-input" type="checkbox" style={{ cursor: "pointer" }} role="switch" id="flexSwitchCheckDefault" />
+                          <label class="form-check-label form-text" for="flexSwitchCheckDefault">Remember me</label>
+                        </div>
+                        <span className="text-primary form-text" style={{ cursor: "pointer" }}> Forget password ?</span>
+                      </div>
+                      <div className="d-grid container">
+                        <button
+                          type="submit"
+                          className={`btn btn-primary me-2 fw-bold ${loading || (LoginData.email == "" && LoginData.password == "") ? "disabled" : null
+                            }`}
+                          onClick={(e) => SubmitLoginForm(e)}
+                        >
+                          Login
+                        </button>
+                      </div>
+                      <h6 className="d-flex justify-content-evenly my-3 form-text"><span>Don't have an account?</span><span className="text-primary" style={{ cursor: "pointer" }} data-bs-target="#SignUpModelToggle2"
+                        data-bs-toggle="modal" >Sign Up</span>
+                      </h6>
+                      <div className="d-flex justify-content-evenly py-4">
+                        <button className=" btn btn-light border rounded-pill"><i class="bi bi-google text-primary px-2"></i>Google</button>
+                        <button className=" btn btn-light border rounded-pill"><i class="bi bi-apple text-secondary px-2"></i>Apple</button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>}
           </div>
